3c5862af802195e9fd5b47e89b2c862e6acf4918
huakon <87414883+huakon@users.noreply.github.com>
2025-01-15 10:09:41 +0900

    更新 config.h

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ const char* WIFI_SSID = "你的WiFi名称";
 const char* WIFI_PASSWORD = "你的WiFi密码";
 
 // Home Assistant 配置
-const char* HASS_URL = "https://hass.hamsterking.xyz/api/states";
+const char* HASS_URL = "https://URL/api/states";
 const char* HASS_TOKEN = "你的长令牌";
 
 // 是否启用 HTTPS 验证（true 为验证证书，false 为跳过验证）
