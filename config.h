44edbdd0004c0bf8efbc0dbde6fe10dca7fcb99d
huakon <87414883+huakon@users.noreply.github.com>
2025-01-15 10:15:28 +0900

    更新 config.h

--- config.h
+++ config.h
@@ -13,4 +13,11 @@ const char* hass_token = "YOUR_HASS_TOKEN";
 // 是否启用 HTTPS 验证（true 为验证证书，false 为跳过验证）
 const bool skipCertificateVerification = false;
 
+//mqtt设置
+const char* mqtt_server = "mqtt.example.com";
+const int mqtt_port = 1883;
+const char* mqtt_topic_temp = "home/sensor/temperature";
+const char* mqtt_topic_humidity = "home/sensor/humidity";
+
+
 #endif // CONFIG_H
